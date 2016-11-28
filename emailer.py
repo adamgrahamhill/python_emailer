3dd04f36929a6e378be21d57b082b87bc0543a75
Adam Hill <adam.graham.hill@gmail.com>
2016-11-28 16:04:12 -0500

    Parse response from API and build message to be returned

--- emailer.py
+++ emailer.py
@@ -29,10 +29,22 @@ def get_schedule():
     return schedule
 
 def get_weather_forecast():
+    #Make request to OpenWeather
     url = 'http://api.openweathermap.org/data/2.5/find?q=Orlando,fl&units=imperial&appid=' + os.environ['APPID']
     weather_request = requests.get(url)
+    
+    #Parse response 
     weather_json = weather_request.json()
-    print(weather_json)
+    desc = weather_json['list'][0]['weather'][0]['description']
+    temp_max = weather_json['list'][0]['main']['temp_max']
+    temp_min = weather_json['list'][0]['main']['temp_min']
+
+    #Build message to be returned
+    forecast = 'The Circus forecast for today is '
+    forecast += desc + ' with a high of ' + str(temp_max) + u'\N{DEGREE SIGN}'
+    forecast += ' and a low of ' + str(temp_min) + u'\N{DEGREE SIGN}' + '.'
+
+    return forecast
 
 def main():
     emails = get_emails()
@@ -41,7 +53,8 @@ def main():
     schedule = get_schedule()
     print(schedule)
 
-    get_weather_forecast()
+    forecast = get_weather_forecast()
+    print(forecast)
 
 main()
 
